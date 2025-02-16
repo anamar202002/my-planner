268acde85d6d888febaed2c91ce6dee881bba5b4
aira <anamariasolanopachon@gmail.com>
2025-02-16 15:14:39 -0500

    modelos hasta subcategoria

--- modelos/modelos.py
+++ modelos/modelos.py
@@ -1,1 +1,18 @@
-from flask_sqlalchemy import SQLALchemy
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Categoria (db.Model):
+    __tablename__ = 'categoria'
+
+    id = db.Column(db.Integer, primary_key = True)
+    nombre = db.Column(db.String(250))
+
+
+class Subcategoria (db.Model):
+    __tablename__ = 'subcategoria'
+
+    id = db.Column(db.Integer, primary_key = True)
+    nombre = db.Column(db.String(250))
+    id_categoria = db.Column(db.Integer, db.ForeignKey('Categoria.id'))
+    
