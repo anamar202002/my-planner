2fe6a71c68d969f0b085441a26a3b91f2fc289e3
aira <a.solanop@uniandes.edu.co>
2025-02-17 19:16:11 -0500

    activar bd

--- app.py
+++ app.py
@@ -3,9 +3,19 @@ from modelos.modelos import db
 
 app = Flask(__name__)
 
-@app.route('/')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///planner.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app_context = app.app_context()
+app_context.push()
+
+db.init_app(app)
+db.create_all()
+
+""" @app.route('/')
 def hello_world():
     return "Hello, World!"
 
 if __name__ == '__main__':
     app.run(debug=True)
+ """
--- modelos/modelos.py
+++ modelos/modelos.py
@@ -14,5 +14,9 @@ class Subcategoria (db.Model):
 
     id = db.Column(db.Integer, primary_key = True)
     nombre = db.Column(db.String(250))
-    id_categoria = db.Column(db.Integer, db.ForeignKey('Categoria.id'))
+    id_categoria = db.Column(db.Integer, db.ForeignKey('categoria.id'))
     
+class Tarea (db.Model):
+    __tablename__='tarea'
+
+    id = db.Column(db.Integer, primary_key = True)
