1bce2ffd87de7672d2ccc3598e94f9d4d310147c
aira <a.solanop@uniandes.edu.co>
2025-02-17 19:38:30 -0500

    base de datos finalizada

--- modelos/modelos.py
+++ modelos/modelos.py
@@ -1,6 +1,18 @@
 from flask_sqlalchemy import SQLAlchemy
+import enum
 
 db = SQLAlchemy()
+
+class EnumImportancia(enum.Enum):
+    BAJA = "Baja"
+    MEDIA = "Media"
+    ALTA = "Alta"
+    CRITICA = "Crítica"
+
+class EnumProgreso (enum.Enum):
+    NO_INICIADA = "No Iniciada"
+    EN_PROGRESO = "En Progreso"
+    COMPLETADA = "Completada"
 
 class Categoria (db.Model):
     __tablename__ = 'categoria'
@@ -20,3 +32,20 @@ class Tarea (db.Model):
     __tablename__='tarea'
 
     id = db.Column(db.Integer, primary_key = True)
+    id_subcategoria = db.Column(db.Integer, db.ForeignKey('subcategoria.id'))
+    nombre = db.Column(db.String(250))
+    descripcion = db.Column(db.String(550))
+    fecha = db.Column(db.DateTime(timezone=True))
+    importancia = db.Column(db.Enum(EnumImportancia), nullable=False)
+    progreso = db.Column(db.Enum(EnumProgreso), nullable=False)
+
+
+class Subarea (db.Model):
+    __tablename__='subtarea'
+
+    id = db.Column(db.Integer, primary_key = True)
+    id_tarea = db.Column(db.Integer, db.ForeignKey('tarea.id'))
+    nombre = db.Column(db.String(250))
+    descripcion = db.Column(db.String(550))
+    fecha = db.Column(db.DateTime(timezone=True))
+    
