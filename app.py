1573f1b54494a1ce6eefbbb83f642a091ab85982
ananamariasolanopachon@gmail.com <ananamariasolanopachon@gmail.com>
2025-06-01 22:57:02 -0500

    subcategorias

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ db.create_all()
 api = Api(app)
 api.add_resource(vistas.VistaCategoria, '/categoria', endpoint = 'crear')
 api.add_resource(vistas.VistaCategoria, '/categoria/<int:id_categoria>', endpoint = 'editar&borrar')
-api.add_resource(vistas.VistaSubcategoria,'/categoria/<int:id_categoria>/subcategoria')
+api.add_resource(vistas.VistaSubcategoria,'/categoria/<int:id_categoria>/subcategoria', endpoint = 'crearSubcategoria')
+api.add_resource(vistas.VistaSubcategoria,'/subcategoria/<int:id_subcategoria>', endpoint = 'editar&borrarSubcategoria')
 
 """ @app.route('/')
 def hello_world():
--- vistas/vistas.py
+++ vistas/vistas.py
@@ -3,21 +3,23 @@ from flask_restful import Resource
 from sqlalchemy.exc import IntegrityError
 
 from modelos import *
-categoria_schama = CategoriaSchema(many=True)
-subcategoria_schama = SubcategoriaSchema(many=True)
+categoria_schama = CategoriaSchema(many=False)
+categorias_schama = CategoriaSchema(many=True)
+subcategoria_schama = SubcategoriaSchema(many=False)
+subcategorias_schama = SubcategoriaSchema(many=True)
 tarea_schama = TareaSchema(many=True)
 subtarea_schama = SubcategoriaSchema(many=True)
 
 class VistaCategoria (Resource):
     def post(self):
-        nueva_categoria = Categoria(nombre=request.json["Nombre"])
+        nueva_categoria = Categoria(nombre=request.json["nombre"])
         db.session.add(nueva_categoria)
         db.session.commit()
         return categoria_schama.dump(nueva_categoria)
     
     def put(self, id_categoria):
         categoria = db.get_or_404(Categoria, id_categoria)
-        categoria.nombre = request.json.get("Nombre", categoria.nombre)
+        categoria.nombre = request.json.get("nombre", categoria.nombre)
         db.session.commit()
         return categoria_schama.dump(categoria)
     
@@ -29,12 +31,28 @@ class VistaCategoria (Resource):
     
     def get (self):
         categorias=db.session.query(Categoria).all()
-        print(categorias)
-        return categoria_schama.dump(categorias)
+        return categorias_schama.dump(categorias)
 
 class VistaSubcategoria (Resource):
     def post(self, id_categoria):
-        nueva_subcategoria = Subcategoria(nombre=request.json["Nombre"], id_categoria=id_categoria)
+        nueva_subcategoria = Subcategoria(nombre=request.json["nombre"], id_categoria=id_categoria)
         db.session.add(nueva_subcategoria)
         db.session.commit()
-        return categoria_schama.dump(nueva_subcategoria)
+        return subcategoria_schama.dump(nueva_subcategoria)
+    
+    def put(self, id_subcategoria):
+        subcategoria = db.get_or_404(Subcategoria, id_subcategoria)
+        subcategoria.nombre = request.json.get("nombre", subcategoria.nombre)
+        subcategoria.id_categoria = request.json.get("id_categoria", subcategoria.id_categoria)
+        db.session.commit()
+        return subcategoria_schama.dump(subcategoria)
+    
+    def delete (self, id_subcategoria):
+        subcategoria = db.get_or_404(Subcategoria, id_subcategoria)
+        db.session.delete(subcategoria)
+        db.session.commit()
+        return '', 204
+    
+    def get (self,id_categoria):
+        subcategorias=db.session.query(Subcategoria).filter_by(id_categoria = id_categoria).all()
+        return subcategorias_schama.dump(subcategorias)
