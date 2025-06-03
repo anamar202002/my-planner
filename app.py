65ab868beca72f9232807dfbc05331df05c02fc1
ananamariasolanopachon@gmail.com <ananamariasolanopachon@gmail.com>
2025-06-02 19:58:45 -0500

    prueba

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ db.create_all()
 api = Api(app)
 api.add_resource(vistas.VistaCategoria, '/categoria', endpoint = 'crear')
 api.add_resource(vistas.VistaCategoria, '/categoria/<int:id_categoria>', endpoint = 'editar&borrar')
-api.add_resource(vistas.VistaSubcategoria,'/categoria/<int:id_categoria>/subcategoria', endpoint = 'crearSubcategoria')
+api.add_resource(vistas.VistaSubcategoria,'/categoria/<int:id_categoria>/subcategoria', endpoint = 'crear&listarSubcategoria')
 api.add_resource(vistas.VistaSubcategoria,'/subcategoria/<int:id_subcategoria>', endpoint = 'editar&borrarSubcategoria')
 
 """ @app.route('/')
